b431bc33058c557202942d38ff43c1ce2ba4f85d
Roland <rolandafaga@gmail.com>
2020-05-11 10:59:06 -1000

    additional check

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
-class HomeHandler(webapp2.RequestHandler):
+class Home (webapp2.RequestHandler):
     def get(self):
         home_template = jinja_env.get_template('pages/index.html')
         self.response.write(home_template.render())
 
-class BucketHandler(webapp2.RequestHandler):
-    def get(self):
-        bucket_template = jinja_env.get_template('pages/bucket.html')
-        self.response.write(bucket_template.render())
-
-class ResumeHandler(webapp2.RequestHandler):
+class Resume (webapp2.RequestHandler):
     def get(self):
         resume_template = jinja_env.get_template('pages/resume.html')
         self.response.write(resume_template.render())
 
+class AboutMe (webapp2.RequestHandler):
+    def get(self):
+        aboutme_template = jinja_env.get_template('pages/about-me.html')
+        self.response.write(aboutme_template.render())
+
+
 app = webapp2.WSGIApplication([
-    ('/', HomeHandler),
-    ('/bucket', BucketHandler),
-    ('/resume', ResumeHandler),
+    ('/', Home),
+    ('/resume', Resume),
+    ('/aboutme', AboutMe),
 ], debug=True)
