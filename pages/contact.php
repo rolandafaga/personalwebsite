62d08428892e16bab44f9318156614859ac5e2d2
Roland Afaga <rolandafaga@gmail.com>
2022-08-13 09:20:26 -1000

    updated links and content

--- pages/contact.php
+++ pages/contact.php
@@ -2,16 +2,14 @@
 
 <html>
 	<head>
-		<!-- Google Analytics -->
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-QPMFCK7WL6"></script>
 		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
 
-			ga('create', 'UA-174251519-1', 'auto');
-			ga('require', 'displayfeatures');
-			ga('send', 'pageview');
+		  gtag('config', 'G-QPMFCK7WL6');
 		</script>
 		<!-- End Google Analytics -->
 		
